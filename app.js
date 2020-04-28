b3cbdcb5961a1f06e286d0795f458a2f571647af
Svetlana Raeva <svetlana.myth@gmail.com>
2020-04-28 15:39:01 -0700

    new path

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/teapot', (req, res) => {
+  res.setHeader('X-My-Header', 42);
+  res.status = 418;
+  res.send('Teapot');
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
